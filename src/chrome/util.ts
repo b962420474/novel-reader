b8b00059ee9518e9092a993ee39f1550a2c227a7
xuexiang.yang <xuexiang.yang@iservernetwork.com>
2023-05-17 13:56:12 +0800

    修复乱码问题

--- src/chrome/util.ts
+++ src/chrome/util.ts
@@ -1,6 +1,8 @@
 import { ipcRenderer } from 'electron'
 import path from 'path'
 import fs from 'fs'
+import iconv from 'iconv-lite'
+import { detect } from 'jschardet'
 import { content, menu } from '@/types/book'
 const send = (event:string, params:any = null) => {
   return new Promise<any>((resolve) => {
@@ -51,29 +53,7 @@ const loadBook = (dir:string) => {
   let contents:string[] = []
   let i = 0
   return new Promise<{menus:menu[], datas:content, title:string}>((resolve, reject) => {
-    const readStream = fs.createReadStream(dir)
-    let char
     const t = Date.now()
-    let last = ''
-    readStream.on('readable', () => {
-      while ((char = readStream.read(1024)) !== null) {
-        const m = Buffer.from(char).toString().split('\n')
-        if (m.length === 1) {
-          last = last + m[0]
-        } else {
-          m[0] = last + m[0]
-          last = m.splice(m.length - 1, 1).join('')
-        }
-        m.forEach(item => writeFileWithLine(item, false))
-      }
-    })
-    readStream.on('end', () => {
-      writeFileWithLine(last, true)
-      console.log((Date.now() - t) / 1000, 's')
-    })
-    readStream.on('error', (err) => {
-      reject(err)
-    })
     const writeFileWithLine = (line:string, isover:boolean) => {
       if (menus.length < 1) {
         i++
@@ -98,9 +78,55 @@ const loadBook = (dir:string) => {
         if (contents.length > 0) {
           datas[i] = contents.join('\n')
         }
+        console.log((Date.now() - t) / 1000, 's')
         resolve({ datas, menus, title: name })
       }
     }
+    readLine(dir, writeFileWithLine, (err) => {
+      reject(err)
+    })
   })
+}
+
+const readLine = (dir:string, fn:(content:string, isOver:boolean)=>void, error:(err:Error)=>void):void => {
+  // const readStream = fs.createReadStream(dir)
+  // let char
+  // const t = Date.now()
+  // let last = ''
+  // readStream.on('readable', () => {
+  //   while ((char = readStream.read(1024)) !== null) {
+  //     const m = Buffer.from(char).toString().split('\n')
+  //     if (m.length === 1) {
+  //       last = last + m[0]
+  //     } else {
+  //       m[0] = last + m[0]
+  //       last = m.splice(m.length - 1, 1).join('')
+  //     }
+  //     m.forEach(item => fn(item, false))
+  //   }
+  // })
+  // readStream.on('end', () => {
+  //   fn(last, true)
+  //   console.log((Date.now() - t) / 1000, 's')
+  // })
+  // readStream.on('error', (err) => {
+  //   error(err)
+  // })
+  fs.readFile(dir, (err, data) => {
+    if (err) {
+      error(err)
+    } else {
+      const encoding = getEncoding(data)
+      console.log('charset:', encoding)
+      const str = iconv.decode(data, encoding)
+      const lines = str.split(/\r?\n/)
+      lines.forEach((line, index) => {
+        fn(line, index === lines.length - 1)
+      })
+    }
+  })
+}
+const getEncoding = (buff:Buffer) => {
+  return detect(buff.slice(0, 128)).encoding
 }
 export { send, importBook, loadBook }
